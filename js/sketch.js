36b3c86d106b4b3a8fa36f16b80aa8a0a02ee64f
nouf <nouf.aljowaysir@havas.com>
2019-09-18 02:22:17 -0400

    viz update

--- js/sketch.js
+++ js/sketch.js
@@ -6,10 +6,11 @@ var cameraSpeed = 0;
 
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2(), INTERSECTED;
+var spheres = [];
 
 var group, textMesh1, textMesh2, textGeo, materials;
 
-const mapping_range = 400.;
+const mapping_range = 200.;
 
 var prevTime = performance.now();
 var velocity = new THREE.Vector3();
@@ -17,6 +18,8 @@ var direction = new THREE.Vector3();
 
 var data;
 var words_array;
+
+var vecs = ['global cash card', 'pay card program', 'wisely pay card', 'adp payroll clients', 'credit card', 'pay card', 'financial wellness', 'early pay', 'pay active', 'payroll card', 'daily pay', 'debit card', 'direct deposit'];
 preload();
 
 function preload() {
@@ -32,28 +35,78 @@ function preload() {
 
 function init() {
   words_array = Object.keys(data);
+  let xyz = {}
   //console.log(words_array);
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000000);
-  camera.position.set(0, -0, 150);
+  camera.position.set(0, -0, 80);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xf0f0f0);
 
-  var plane = new THREE.GridHelper(400, 50);
-  plane.position.set(0,-20,0);
-  scene.add(plane);
+  for (let j = 0; j < vecs.length; j++) {
+    if ( words_array.includes(vecs[j]) ) {
+      let index = words_array.indexOf(vecs[j]);
+      words_array.splice(index,1)
+
+      let message = vecs[j];
+      let material, color;
+
+      color = 0xff69b4
+      material = new THREE.MeshPhongMaterial( {
+        color: color,
+        specular: 0x050505,
+        shininess: 100,
+        opacity: 0.5
+      });
+      let geo = new THREE.SphereGeometry( 0.5, 32, 32 );
+      let x = (data[message][0][0]);
+      let y = (data[message][0][1]);
+      let z = (data[message][0][2]);
+
+      let mappedX = Math.floor(mapping(x, -17., 18., -mapping_range, mapping_range));
+      let mappedY = Math.floor(mapping(y, -17., 18., -mapping_range, mapping_range));
+      let mappedZ = Math.floor(mapping(z, -17., 18., -mapping_range/2., mapping_range));
+
+      if (j == 2) {
+        xyz["x"] = mappedX
+        xyz["y"] = mappedY
+        xyz["z"] = mappedZ
+      }
+
+      let sphere = new THREE.Mesh( geo, material );
+      sphere.position.set(mappedX,mappedY,mappedZ)
+      sphere.text = vecs[j]
+
+      scene.add( sphere );
+      spheres.push( sphere );
+    }
+  }
   scene.fog = new THREE.Fog(0xf0f0f0, 0, 850);
 
   scenelight = new THREE.AmbientLight(0x404040);
   scene.add(scenelight);
 
+  var axesHelper = new THREE.AxesHelper( 20 );
+  console.log(xyz["x"])
+  axesHelper.position.set(xyz["x"],xyz["y"],xyz["z"]);
+  scene.add( axesHelper );
+
   var loader = new THREE.FontLoader();
 
   loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-    for (var i = 0; i < words_array.length; i++) {
+    for (let i = 0; i < words_array.length; i++) {
       let message = words_array[i];
-      let geo = new THREE.SphereGeometry( 0.5, 32, 32 );
+      let material, color;
 
+      color = 0x000000
+      material = new THREE.MeshPhongMaterial( {
+        color: color,
+        specular: 0x050505,
+        shininess: 100,
+        opacity: 0.3,
+        transparent: true
+      });
+      let geo = new THREE.SphereGeometry( 0.3, 32, 32 );
       let x = (data[message][0][0]);
       let y = (data[message][0][1]);
       let z = (data[message][0][2]);
@@ -63,33 +116,28 @@ function init() {
       let mappedZ = Math.floor(mapping(z, -17., 18., -mapping_range/2., mapping_range));
 
       let sphere = new THREE.Mesh( geo, material );
-      let color = data[message][1];
-      // var material = new THREE.MeshPhongMaterial({
-      //   color: color,
-      //   shininess: 35,
-      //
-      //   transparent: true,
-      //   side: THREE.DoubleSide
-      // });
-      var material = new THREE.MeshPhongMaterial( {
-        color: color,
-        specular: 0x050505,
-        shininess: 100
-      })
       sphere.position.set(mappedX,mappedY,mappedZ)
-      sphere.type = "sphere"
+      sphere.text = message
 
       scene.add( sphere );
+      spheres.push( sphere );
 
-      var shapes = font.generateShapes( message, 0.25 );
-      var geometry = new THREE.ShapeBufferGeometry( shapes );
-      geometry.computeBoundingBox();
-      xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
-      geometry.translate( xMid, 0, 0 );
-      // make shape ( N.B. edge view not visible )
-      text = new THREE.Mesh( geometry, material );
-      text.position.set(mappedX,mappedY-2,mappedZ);
-      scene.add( text );
+
+      // for (let i = 0; i < vecs.length; i++) {
+      //   if (message == vecs[i][0]) {
+      //     var shapes = font.generateShapes( message, 0.25 );
+      //     var geometry = new THREE.ShapeBufferGeometry( shapes );
+      //     geometry.computeBoundingBox();
+      //     xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+      //     geometry.translate( xMid, 0, 0 );
+      //     // make shape ( N.B. edge view not visible )
+      //     text = new THREE.Mesh( geometry, material );
+      //     text.position.set(mappedX,mappedY-2,mappedZ);
+      //     scene.add( text );
+      //   } else {
+      //     geo = new THREE.SphereGeometry( 0.2, 32, 32 );
+      //   }
+      // }
     }
   });
 
@@ -112,54 +160,29 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
 
-  // raycaster.setFromCamera( mouse, camera );
-  // var intersects = raycaster.intersectObjects( scene.children );
-  // if ( intersects.length > 0 ) {
-  //   console.log(intersects)
-  //   if ( INTERSECTED != intersects[ 0 ].object ) {
-  //     if ( INTERSECTED &&  intersects[ 0 ].object.type == "sphere")
-  //     INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-  //     INTERSECTED = intersects[ 0 ].object;
-  //     INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-  //     INTERSECTED.material.emissive.setHex( 0xff0000 );
-  //   }
-  // } else {
-  //   if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-  //   INTERSECTED = null;
-  // }
+  raycaster.setFromCamera( mouse, camera );
+  var intersects = raycaster.intersectObjects( spheres );
+  if ( intersects.length > 0 ) {
+    if ( INTERSECTED != intersects[ 0 ].object ) {
+      if ( INTERSECTED)
+      INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+      INTERSECTED = intersects[ 0 ].object;
+      INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+      INTERSECTED.material.emissive.setHex( 0xff0000 );
+      let element = document.getElementById("txt");
+      element.innerHTML = INTERSECTED.text;
+
+    }
+  } else {
+    if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+    INTERSECTED = null;
+  }
 
   render();
 }
 
 function render() {
   controls.update();
-  // var _time = Date.now() * 0.001;
-  // if (controlsEnabled === true) {
-  //
-  //     var time = performance.now();
-  //     var delta = (time - prevTime) / 1000;
-  //     velocity.x -= velocity.x * 5.0 * delta;
-  //     velocity.z -= velocity.z * 1.0 * delta;
-  //     velocity.y -= velocity.y *5.0 *delta;
-  //     // velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-  //     direction.z = Number(moveForward) - Number(moveBackward);
-  //     direction.x = Number(moveLeft) - Number(moveRight);
-  //     direction.y = Number(moveDown) - Number(moveUp);
-  //     direction.normalize(); // this ensures consistent movements in all directions
-  //     if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
-  //     if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
-  //     if(moveUp||moveDown) velocity.y -= direction.y *400 * delta;
-  //
-  //     controls.getObject().translateX(velocity.x * delta);
-  //     controls.getObject().translateY(velocity.y * delta);
-  //     controls.getObject().translateZ(velocity.z * delta);
-  //     // if (controls.getObject().position.y < 10) {
-  //     //     velocity.y = 0;
-  //     //     controls.getObject().position.y = 10;
-  //     // }
-  //     prevTime = time;
-  // }
-
   renderer.render(scene, camera);
 }
 
