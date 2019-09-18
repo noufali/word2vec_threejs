68c4092ed1e824ce9cab066e083d8fa077db2bad
nouf <nouf.aljowaysir@havas.com>
2019-09-18 13:54:11 -0400

    viz update

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,5 @@
 
-const path_to_file = "vectors/data_3d_vector_result2.json";
+const path_to_file = "vectors/data_3d_vector_result3.json";
 
 var camera, scene, group, scenelight, renderer, controls;
 var cameraSpeed = 0;
@@ -19,7 +19,6 @@ var direction = new THREE.Vector3();
 var data;
 var words_array;
 
-var vecs = ['global cash card', 'pay card program', 'wisely pay card', 'adp payroll clients', 'credit card', 'pay card', 'financial wellness', 'early pay', 'pay active', 'payroll card', 'daily pay', 'debit card', 'direct deposit'];
 preload();
 
 function preload() {
@@ -38,19 +37,14 @@ function init() {
   let xyz = {}
   //console.log(words_array);
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000000);
-  camera.position.set(0, -0, 80);
+  camera.position.set(-5, -5, 60);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xf0f0f0);
 
-  for (let j = 0; j < vecs.length; j++) {
-    if ( words_array.includes(vecs[j]) ) {
-      let index = words_array.indexOf(vecs[j]);
-      words_array.splice(index,1)
-
-      let message = vecs[j];
-      let material, color;
-
+  for (let i = 0; i < words_array.length; i++) {
+    let word = words_array[i];
+    if (data[word].keyword == true) {
       color = 0xff69b4
       material = new THREE.MeshPhongMaterial( {
         color: color,
@@ -58,24 +52,47 @@ function init() {
         shininess: 100,
         opacity: 0.5
       });
-      let geo = new THREE.SphereGeometry( 0.5, 32, 32 );
-      let x = (data[message][0][0]);
-      let y = (data[message][0][1]);
-      let z = (data[message][0][2]);
+      let geo = new THREE.SphereGeometry( 0.18, 32, 32 );
+      let x = (data[word].vec[0]);
+      let y = (data[word].vec[1]);
+      let z = (data[word].vec[2]);
 
       let mappedX = Math.floor(mapping(x, -17., 18., -mapping_range, mapping_range));
       let mappedY = Math.floor(mapping(y, -17., 18., -mapping_range, mapping_range));
       let mappedZ = Math.floor(mapping(z, -17., 18., -mapping_range/2., mapping_range));
 
-      if (j == 2) {
-        xyz["x"] = mappedX
-        xyz["y"] = mappedY
-        xyz["z"] = mappedZ
+      let sphere = new THREE.Mesh( geo, material );
+      sphere.position.set(mappedX,mappedY,mappedZ)
+      sphere.text = word
+
+      scene.add( sphere );
+      spheres.push( sphere );
+      if (word = "global cash card") {
+        xyz["x"] = mappedX;
+        xyz["y"] = mappedY;
+        xyz["z"] = mappedZ;
       }
+    } else {
+      color = 0x000000
+      material = new THREE.MeshPhongMaterial( {
+        color: color,
+        specular: 0x050505,
+        shininess: 100,
+        opacity: 0.3,
+        transparent: true
+      });
+      let geo = new THREE.SphereGeometry( 0.09, 32, 32 );
+      let x = (data[word].vec[0]);
+      let y = (data[word].vec[1]);
+      let z = (data[word].vec[2]);
+
+      let mappedX = Math.floor(mapping(x, -17., 18., -mapping_range, mapping_range));
+      let mappedY = Math.floor(mapping(y, -17., 18., -mapping_range, mapping_range));
+      let mappedZ = Math.floor(mapping(z, -17., 18., -mapping_range/2., mapping_range));
 
       let sphere = new THREE.Mesh( geo, material );
       sphere.position.set(mappedX,mappedY,mappedZ)
-      sphere.text = vecs[j]
+      sphere.text = word
 
       scene.add( sphere );
       spheres.push( sphere );
@@ -86,60 +103,60 @@ function init() {
   scenelight = new THREE.AmbientLight(0x404040);
   scene.add(scenelight);
 
-  var axesHelper = new THREE.AxesHelper( 20 );
+  var axesHelper = new THREE.AxesHelper( 10 );
   console.log(xyz["x"])
   axesHelper.position.set(xyz["x"],xyz["y"],xyz["z"]);
   scene.add( axesHelper );
 
-  var loader = new THREE.FontLoader();
-
-  loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-    for (let i = 0; i < words_array.length; i++) {
-      let message = words_array[i];
-      let material, color;
-
-      color = 0x000000
-      material = new THREE.MeshPhongMaterial( {
-        color: color,
-        specular: 0x050505,
-        shininess: 100,
-        opacity: 0.3,
-        transparent: true
-      });
-      let geo = new THREE.SphereGeometry( 0.3, 32, 32 );
-      let x = (data[message][0][0]);
-      let y = (data[message][0][1]);
-      let z = (data[message][0][2]);
-
-      let mappedX = Math.floor(mapping(x, -17., 18., -mapping_range, mapping_range));
-      let mappedY = Math.floor(mapping(y, -17., 18., -mapping_range, mapping_range));
-      let mappedZ = Math.floor(mapping(z, -17., 18., -mapping_range/2., mapping_range));
-
-      let sphere = new THREE.Mesh( geo, material );
-      sphere.position.set(mappedX,mappedY,mappedZ)
-      sphere.text = message
-
-      scene.add( sphere );
-      spheres.push( sphere );
-
-
-      // for (let i = 0; i < vecs.length; i++) {
-      //   if (message == vecs[i][0]) {
-      //     var shapes = font.generateShapes( message, 0.25 );
-      //     var geometry = new THREE.ShapeBufferGeometry( shapes );
-      //     geometry.computeBoundingBox();
-      //     xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
-      //     geometry.translate( xMid, 0, 0 );
-      //     // make shape ( N.B. edge view not visible )
-      //     text = new THREE.Mesh( geometry, material );
-      //     text.position.set(mappedX,mappedY-2,mappedZ);
-      //     scene.add( text );
-      //   } else {
-      //     geo = new THREE.SphereGeometry( 0.2, 32, 32 );
-      //   }
-      // }
-    }
-  });
+  // var loader = new THREE.FontLoader();
+  //
+  // loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+  //   for (let i = 0; i < words_array.length; i++) {
+  //     let message = words_array[i];
+  //     let material, color;
+  //
+  //     color = 0x000000
+  //     material = new THREE.MeshPhongMaterial( {
+  //       color: color,
+  //       specular: 0x050505,
+  //       shininess: 100,
+  //       opacity: 0.3,
+  //       transparent: true
+  //     });
+  //     let geo = new THREE.SphereGeometry( 0.3, 32, 32 );
+  //     let x = (data[message][0][0]);
+  //     let y = (data[message][0][1]);
+  //     let z = (data[message][0][2]);
+  //
+  //     let mappedX = Math.floor(mapping(x, -17., 18., -mapping_range, mapping_range));
+  //     let mappedY = Math.floor(mapping(y, -17., 18., -mapping_range, mapping_range));
+  //     let mappedZ = Math.floor(mapping(z, -17., 18., -mapping_range/2., mapping_range));
+  //
+  //     let sphere = new THREE.Mesh( geo, material );
+  //     sphere.position.set(mappedX,mappedY,mappedZ)
+  //     sphere.text = message
+  //
+  //     scene.add( sphere );
+  //     spheres.push( sphere );
+  //
+  //
+  //     // for (let i = 0; i < vecs.length; i++) {
+  //     //   if (message == vecs[i][0]) {
+  //     //     var shapes = font.generateShapes( message, 0.25 );
+  //     //     var geometry = new THREE.ShapeBufferGeometry( shapes );
+  //     //     geometry.computeBoundingBox();
+  //     //     xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+  //     //     geometry.translate( xMid, 0, 0 );
+  //     //     // make shape ( N.B. edge view not visible )
+  //     //     text = new THREE.Mesh( geometry, material );
+  //     //     text.position.set(mappedX,mappedY-2,mappedZ);
+  //     //     scene.add( text );
+  //     //   } else {
+  //     //     geo = new THREE.SphereGeometry( 0.2, 32, 32 );
+  //     //   }
+  //     }
+  //   }
+  // });
 
   renderer = new THREE.WebGLRenderer({});
   renderer.setPixelRatio(window.devicePixelRatio);
