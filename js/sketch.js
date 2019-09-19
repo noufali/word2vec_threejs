5eb64383e86625a824d0cad47693316bb65e447a
nouf <nouf.aljowaysir@havas.com>
2019-09-19 11:30:13 -0400

    camera update

--- js/sketch.js
+++ js/sketch.js
@@ -36,8 +36,8 @@ function preload() {
 function init() {
   words_array = Object.keys(data);
   let xyz = {}
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000000);
-  camera.position.set(-7, -3, 48);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(-5, 1, 51);
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xf0f0f0);
@@ -215,7 +215,7 @@ bindButtons();
 
 function animate() {
   requestAnimationFrame(animate);
-  //console.log(camera.position);
+  console.log(camera.position);
 
   raycaster.setFromCamera( mouse, camera );
   var intersects = raycaster.intersectObjects( spheres );
